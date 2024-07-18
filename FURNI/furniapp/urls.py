10075e6aebdb1c56f57d77d3cbbce34a0b3ae8a7
shetkarmitesh <miteshshetkar3125@gmail.com>
2024-07-19 04:18:41 +0530

    urls mapping

--- FURNI/furniapp/urls.py
+++ FURNI/furniapp/urls.py
@@ -4,6 +4,15 @@ from . import views
 
 urlpatterns = [
      path('', views.index,name="index"),
+     path('about', views.about,name="about"),
+     path('shop', views.shop,name="shop"),
+     path('contact', views.contact,name="contact"),
+     path('blog', views.blog,name="blog"),
+     path('cart', views.cart,name="cart"),
+     path('services', views.services,name="services"),
+     path('checkout', views.checkout,name="checkout"),
+     path('thankyou', views.thankyou,name="thankyou")
+     
 ]
 
 
--- FURNI/furniapp/views.py
+++ FURNI/furniapp/views.py
@@ -28,3 +28,24 @@ def index(request):
 
     furni= furniture.objects.all()
     return render(request,'index.html',{'furni':furni})
+
+def about(request):
+    return render(request,'about.html')
+
+def shop(request):
+    return render(request,'shop.html')
+
+def services(request):
+    return render(request,'services.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def contact(request):
+    return render(request,'contact.html')
+def cart(request):
+    return render(request,'cart.html')
+def checkout(request):
+    return render(request,'checkout.html')
+def thankyou(request):
+    return render(request,'thankyou.html')
